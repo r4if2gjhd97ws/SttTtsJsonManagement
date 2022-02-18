3353da8dbf5e1f4268877383461fdf4c8f359fa1
yohei_takasaki <y.takasaki@speakbuddy.jp>
2022-02-19 02:29:16 +0900

    title

--- SttTtsJsonManagement/src/app/app.component.ts
+++ SttTtsJsonManagement/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'SttTtsJsonManagement';
+  title = 'stt / tts replacement';
+  isAngular = true
 }
